336f1f71b1bafae21bff8d9ff6716a36a5c9990b
Nicolas Girot <ngirot@hotmail.com>
2024-08-19 17:28:15 +0200

    Use real earth/moon/sun positions in real time

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 import p5 from 'p5';
+import {Body, HelioVector} from "astronomy-engine"
 
-const moonPosition = {x:  100, y: 250, z: 500};
-const earthPosition = {x: 200, y: 400, z: 0};
-const sunPosition = {x: 0, y: 0, z: 0};
+
+const toCoord = function (vector) {
+  const ua = 1000;
+  return {x: -vector.x * ua, y: vector.z * ua, z: vector.y * ua};
+}
 
 const sketch = function (p) {
   let shaders;
@@ -20,12 +23,23 @@ const sketch = function (p) {
     //p.noLoop();
   }
 
-  p.draw = function () {
+  p.draw = async function () {
+    const date = new Date();
+    date.setMilliseconds(0);
+
+    const moonVector = new HelioVector(Body.Moon, date);
+    const earthVector = new HelioVector(Body.Earth, date);
+    const sunVector = new HelioVector(Body.Sun, date);
+
+    const moonPosition = toCoord(moonVector);
+    const earthPosition = toCoord(earthVector);
+    const sunPosition = toCoord(sunVector);
+
     p.background(0);
 
     const cam = p.createCamera();
     cam.setPosition(earthPosition.x, earthPosition.y, earthPosition.z);
-    cam.perspective(1, 1, 0.01, 10000000000000);
+    cam.perspective(0.024, 1, 0.01, 10000000000000);
     cam.lookAt(moonPosition.x, moonPosition.y, moonPosition.z);
 
     shaders.setUniform("uLightPosition", [sunPosition.x, sunPosition.y, sunPosition.z]);
@@ -34,7 +48,7 @@ const sketch = function (p) {
 
     p.translate(moonPosition.x, moonPosition.y, moonPosition.z);
     p.rotate(p.PI);
-    p.sphere(100, 100, 100);
+    p.sphere(0.02322760333, 100, 100);
   }
 };
 
