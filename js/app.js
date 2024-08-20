84fdde7a751a77fe3c6a4eed0d6e643a4c14d12b
Nicolas Girot <ngirot@hotmail.com>
2024-08-20 09:44:34 +0200

    Add moon rotation

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 import p5 from 'p5';
-import {Body, HelioVector} from "astronomy-engine"
+import {Body, HelioVector, RotationAxis} from "astronomy-engine"
 
 
 const toCoord = function (vector) {
-  const ua = 1000;
-  return {x: -vector.x * ua, y: vector.z * ua, z: vector.y * ua};
+  const factor = 1000;
+  return {x: -vector.x * factor, y: vector.z * factor, z: vector.y * factor};
 }
 
 const sketch = function (p) {
@@ -20,14 +20,20 @@ const sketch = function (p) {
     p.createCanvas(800, 800, p.WEBGL);
     p.shader(shaders);
     p.noStroke();
+
+    p.angleMode(p.RADIANS)
     //p.noLoop();
   }
 
   p.draw = async function () {
     const date = new Date();
+    //date.setHours(date.getHours() + (date.getTime() / 55) % 100000);
+    //date.setHours(date.getHours() + 8);
     date.setMilliseconds(0);
 
+
     const moonVector = new HelioVector(Body.Moon, date);
+    const moonRotation = RotationAxis(Body.Moon, date);
     const earthVector = new HelioVector(Body.Earth, date);
     const sunVector = new HelioVector(Body.Sun, date);
 
@@ -47,7 +53,9 @@ const sketch = function (p) {
     shaders.setUniform('time', p.millis());
 
     p.translate(moonPosition.x, moonPosition.y, moonPosition.z);
-    p.rotate(p.PI);
+
+    p.rotate((moonRotation.spin % 360) * p.PI / 180, [moonRotation.north.x, moonRotation.north.z, moonRotation.north.y]);
+    p.rotate(p.PI/2, [0, 1, 0])
     p.sphere(0.02322760333, 100, 100);
   }
 };
