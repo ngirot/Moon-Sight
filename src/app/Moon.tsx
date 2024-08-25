7984b6a445901fd0257e2360feae98bea7b4ef39
Nicolas Girot <ngirot@hotmail.com>
2024-08-26 00:08:34 +0200

    Add simple date picker to change date of perspective

--- src/app/Moon.tsx
+++ src/app/Moon.tsx
@@ -1,73 +1,90 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useState} from "react";
 import p5, {Image, Shader} from "p5"
 import {Body, HelioVector, RotationAxis, Vector} from "astronomy-engine";
 import {P5Wrapper} from "@/app/P5Wrapper";
+import {Moment} from "moment";
 
-let sketchable: p5;
+interface MoonProps {
+    date: Moment;
+}
+
+interface BuildWrapper {
+    date: Date;
+}
 
 const toCoord = function (vector: Vector) {
     const factor = 1000;
     return {x: -vector.x * factor, y: vector.z * factor, z: vector.y * factor};
 }
 
-function sketch(p: p5) {
-    let shaders: Shader;
-    let moonTexture: Image;
+function build(wrapper: BuildWrapper) {
+    return (p: p5) => {
+        let shaders: Shader;
+        let moonTexture: Image;
 
-    const toDegree = function (rad: number): number {
-        return (rad % 360) * p.PI / 180;
-    }
+        const toDegree = function (rad: number): number {
+            return (rad % 360) * p.PI / 180;
+        }
 
-    p.preload = function () {
-        shaders = p.loadShader('shaders/shader.vert', 'shaders/shader.frag');
-        moonTexture = p.loadImage("img/lroc_color_poles_4k.jpg");
-    }
+        p.preload = function () {
+            shaders = p.loadShader('shaders/shader.vert', 'shaders/shader.frag');
+            moonTexture = p.loadImage("img/lroc_color_poles_4k.jpg");
+        }
 
-    p.setup = function () {
-        p.createCanvas(800, 800, p.WEBGL);
-        p.shader(shaders);
-        p.noStroke();
+        p.setup = function () {
+            p.createCanvas(800, 800, p.WEBGL);
+            p.shader(shaders);
+            p.noStroke();
 
-        p.angleMode(p.RADIANS)
-        //p.noLoop();
-    }
+            p.angleMode(p.RADIANS)
+            //p.noLoop();
+        }
 
-    p.draw = function () {
-        const date = new Date();
-        //date.setHours(date.getHours() + (date.getTime() / 55) % 100000);
-        //date.setHours(date.getHours() + 8);
-        date.setMilliseconds(0);
+        p.draw = function () {
+            //console.log('d', date);
+            const date2 = wrapper.date;// date.toDate();
+            //date.setHours(date.getHours() + (date.getTime() / 55) % 100000);
+            //date.setHours(date.getHours() + 8);
+            date2.setMilliseconds(0);
 
 
-        const moonVector = HelioVector(Body.Moon, date);
-        const moonRotation = RotationAxis(Body.Moon, date);
-        const earthVector = HelioVector(Body.Earth, date);
-        const sunVector = HelioVector(Body.Sun, date);
+            const moonVector = HelioVector(Body.Moon, date2);
+            const moonRotation = RotationAxis(Body.Moon, date2);
+            const earthVector = HelioVector(Body.Earth, date2);
+            const sunVector = HelioVector(Body.Sun, date2);
 
-        const moonPosition = toCoord(moonVector);
-        const earthPosition = toCoord(earthVector);
-        const sunPosition = toCoord(sunVector);
+            const moonPosition = toCoord(moonVector);
+            const earthPosition = toCoord(earthVector);
+            const sunPosition = toCoord(sunVector);
 
-        p.background(0);
+            p.background(0);
 
-        const cam = p.createCamera();
-        cam.setPosition(earthPosition.x, earthPosition.y, earthPosition.z);
-        cam.perspective(0.024, 1, 0.01, 10000000000000);
-        cam.lookAt(moonPosition.x, moonPosition.y, moonPosition.z);
+            const cam = p.createCamera();
+            cam.setPosition(earthPosition.x, earthPosition.y, earthPosition.z);
+            // @ts-expect-error
+            cam.perspective(0.024, 1, 0.01, 10000000000000);
+            cam.lookAt(moonPosition.x, moonPosition.y, moonPosition.z);
 
-        shaders.setUniform("uLightPosition", [sunPosition.x, sunPosition.y, sunPosition.z]);
-        shaders.setUniform("uTexture", moonTexture);
-        shaders.setUniform('time', p.millis());
+            shaders.setUniform("uLightPosition", [sunPosition.x, sunPosition.y, sunPosition.z]);
+            shaders.setUniform("uTexture", moonTexture);
+            shaders.setUniform('time', p.millis());
 
-        p.translate(moonPosition.x, moonPosition.y, moonPosition.z);
+            p.translate(moonPosition.x, moonPosition.y, moonPosition.z);
 
-        p.rotate(toDegree(moonRotation.spin), [moonRotation.north.x, moonRotation.north.z, moonRotation.north.y]);
-        p.rotate(toDegree(-moonRotation.ra), [1, 0, 0]);
-        p.rotate(p.PI / 2, [0, 1, 0])
-        p.sphere(0.02322760333, 100, 100);
+            p.rotate(toDegree(moonRotation.spin), [moonRotation.north.x, moonRotation.north.z, moonRotation.north.y]);
+            p.rotate(toDegree(-moonRotation.ra), [1, 0, 0]);
+            p.rotate(p.PI / 2, [0, 1, 0])
+            p.sphere(0.02322760333, 100, 100);
+        }
     }
 }
 
-export function Moon() {
-    return <P5Wrapper sketchFn={sketch}></P5Wrapper>;
+export function Moon({date}: MoonProps) {
+    const [wrapper, setWrapper] = useState({date: date.toDate()})
+    console.log('render moon', date);
+    useEffect(() => {
+        wrapper.date = date.toDate();
+    }, [date]);
+
+    return <P5Wrapper sketchFn={build(wrapper)}></P5Wrapper>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,34 @@
 'use client';
 
-import Image from "next/image";
-import styles from "./page.module.css";
-import {App2} from "@/app/comp.";
 import {Moon} from "@/app/Moon";
+import {LocalizationProvider, StaticDateTimePicker} from "@mui/x-date-pickers";
+import {AdapterMoment} from '@mui/x-date-pickers/AdapterMoment'
+import {Grid} from "@mui/material";
+import moment, {Moment} from "moment";
+import dynamic from "next/dynamic";
+import {useState} from "react";
 
 export default function Home() {
+    let [date, setDate] = useState<Moment>(moment());
+
+    const updateDate = (d: Moment | null) => {
+        if (d !== null) {
+            setDate(d);
+        }
+    }
+
     return (
         <div>
-            <Moon></Moon>
+            <Grid container spacing={2}>
+                <Grid item xs={8}>
+                    <Moon date={date}></Moon>
+                </Grid>
+                <Grid item xs={4}>
+                    <LocalizationProvider dateAdapter={AdapterMoment}>
+                        <StaticDateTimePicker value={date} onChange={updateDate}/>
+                    </LocalizationProvider>
+                </Grid>
+            </Grid>
         </div>
     );
 }
