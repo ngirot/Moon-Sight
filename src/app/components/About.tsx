d418e1dc0d76e2098ccd28c1d8e55a179d27b92e
Nicolas Girot <ngirot@hotmail.com>
2024-09-24 02:04:14 +0200

    Add About section

---
+++ src/app/components/About.tsx
@@ -0,0 +1,69 @@
+import {AppBar, Container, Dialog, IconButton, Slide, Toolbar, Typography} from "@mui/material";
+import {forwardRef} from "react";
+import {TransitionProps} from '@mui/material/transitions';
+import {CloseIcon} from "next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon";
+import {Box} from "@mui/system";
+
+interface AboutProps {
+    open: boolean
+    close: () => void
+}
+
+const Transition = forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any>;
+    },
+    ref: React.Ref<unknown>,
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function About({open, close}: AboutProps) {
+    return <Dialog fullScreen={true} open={open} onClose={close} TransitionComponent={Transition}>
+        <AppBar sx={{position: 'relative'}}>
+            <Toolbar>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    onClick={close}
+                    aria-label="close"
+                >
+                    <CloseIcon/>
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                    About
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <Container>
+            <Box>
+                <Typography variant="body1" mt={2}>
+                    Welcome to Moon Sight, your interactive portal for exploring the moon as it appears in the sky from
+                    any location on Earth, at any date and time. Whether you're a casual observer or an avid astronomer,
+                    Moon Sight provides a real-time visualization of the moon's phase, position, and visibility,
+                    customized
+                    for your specific location and time zone. You can also look back to see the moon's past phases or
+                    forward
+                    to see how it will appear in the future.
+                </Typography>
+                <Typography variant="body1" mt={2}>
+                    However, please note that our system does not manage horizon
+                    effects yet, meaning the tool does not account for the moon's rise or set times, nor whether the
+                    moon is
+                    visible above the local horizon at a given time. Moon Sight will show you the moon's position as if
+                    viewed from the celestial sphere, even if it is technically below the horizon in your area.
+                </Typography>
+                <Typography variant="body1" mt={2}>
+                    Additionally, lunar eclipses are not managed by our system, so the moon’s appearance during
+                    these events will not be reflected accurately.
+                </Typography>
+                <Typography variant="body1" mt={2}>
+                    Moon Sight is perfect for everyone from casual stargazers to seasoned astronomers, offering a
+                    stunning
+                    and accurate view of the moon's journey across the sky. Discover the beauty of the moon from any
+                    point on Earth, and explore its phases and movements throughout time. 🌙
+                </Typography>
+            </Box>
+        </Container>
+    </Dialog>
+}
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import {Box} from "@mui/system";
 import {AppBar, Button, IconButton, Toolbar, Typography} from "@mui/material";
 import Image from "next/image";
-
-const pages = ['Home', 'About', 'Contact'];
+import {useState} from "react";
+import About from "@/app/components/About";
 
 export function NavBar() {
+    const [aboutOpen, setAboutOpen] = useState(false);
+
     return <Box sx={{flexGrow: 1}} paddingBottom={2}>
         <AppBar position="static">
             <Toolbar>
@@ -20,14 +22,18 @@ export function NavBar() {
                 <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                     Moon Sight
                 </Typography>
-                {pages.map((page) => (
-                    <Button
-                        key={page}
-                        sx={{my: 2, color: 'white', display: 'block'}}
-                    >
-                        {page}
-                    </Button>
-                ))}
+                <Button
+                    sx={{my: 2, color: 'white', display: 'block'}}
+                    onClick={() => setAboutOpen(true)}
+                >
+                    About
+                </Button>
+                <About open={aboutOpen} close={() => setAboutOpen(false)}></About>
+                <Button
+                    sx={{my: 2, color: 'white', display: 'block'}}
+                >
+                    Contact
+                </Button>
             </Toolbar>
         </AppBar>
     </Box>
